b53aba89393c5d0a1efa163d1d7a49c2daf94de0
Бобров Анатолий <bobroff.tolia2018@yandex.ru>
2026-08-27 16:06:51 +0300

    Initial version of chnet

---
+++ Layer.java
@@ -0,0 +1,13 @@
+import java.util.Random;
+
+public class Layer{
+    private float[] input;
+    private float[] output;
+    private float[] weights;
+
+    public float Output(float[] _input){
+        input = _input;
+
+    }
+
+}
